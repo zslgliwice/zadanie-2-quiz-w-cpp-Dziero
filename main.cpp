be68f9ed307d5f6074b01ba2d31019d603a1c789
Adam Dziero <Dzieroks@gmail.com>
2023-10-23 23:47:34 +0200

    Refactored

--- main.cpp
+++ main.cpp
@@ -1,54 +1,71 @@
 #include <iostream>
-#include <windows.h>
 #include <cstdlib>
+#include <ctime>
+#include <string>
 
 using namespace std;
 
-string imie;
-string odp;
+bool askQuestion(const string& question, const string& correctAnswer) {
+    string userAnswer;
+    cout << question << endl;
+    cout << "Twoja odpowiedz: ";
+    cin >> userAnswer;
+    system("cls");
 
-int main()
-{
-    //https://cpp0x.pl/kursy/Kurs-WinAPI-C++/Roznosci/Kolory-w-konsoli/374
-    HANDLE uchwyt = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(uchwyt, 12);
-    cout << "\aPodaj imie: ";
+    if (userAnswer == correctAnswer) {
+        cout << "Poprawna odpowiedz!" << endl;
+        return true;
+    } else {
+        cout << "Zle! Poprawna odpowiedz to: " << correctAnswer << endl;
+        return false;
+    }
+}
+
+int main() {
+
+    string imie;
+    cout << "Podaj imie: ";
     cin >> imie;
+    system("cls");
+
     int pkt = 0;
 
-    system("cls");
-    cout<< "3";
-    Sleep(1000);
-    system("cls");
-    cout<< "3..2";
-    Sleep(1000);
-    system("cls");
-    cout<< "3..2..1..";
-    Sleep(1000);
-    system("cls");
+const string questions[] = {
+    "Co to jest C++?",
+    "Ile wynosi 2 + 2?",
+    "Jak nazywa sie stolica Francji?",
+    "Kto napisal dramat Romeo i Julia?",
+    "Ktora planeta jest czwarta w ukladzie slonecznym?",
+    "Jak nazywa sie najwieksza rzeka na swiecie?",
+    "Kto jest autorem Mona Lisy?",
+    "Co to jest fotosynteza?",
+    "Ile wynosi pierwiastek kwadratowy z 16?",
+    "Ktory pierwiastek chemiczny ma symbol 'H'?",
+    "Jakie panstwo jest znane jako 'Kraina Wschodzacego Slonca'?"
+};
 
-    cout << imie << " witaj w tescie z programowania\n";
+const string correctAnswers[] = {
+    "Jezyk programowania",
+    "4",
+    "Paryz",
+    "William Shakespeare",
+    "Mars",
+    "Nil",
+    "Leonardo da Vinci",
+    "Proces chemiczny w roslinach",
+    "4",
+    "Wodor",
+    "Japonia"
+};
 
-    cout << "Co to jest C++?"<<endl;
-    cout << "a) Piosenkarka"<<endl;
-    cout << "b) Jezyk programowania"<<endl;
-    cout << "c) Mlynek do herbaty"<<endl;
-    cout << "Twoja odpowiedz: ";
-    cin >> odp;
-    if(odp == "b")
-    {
-        cout << "Poprawna odpowiedz!";
-        pkt++;
+    for (int i = 0; i < 5; ++i) {
+        int randomIndex = rand() % 5;
+        if (askQuestion(questions[randomIndex], correctAnswers[randomIndex])) {
+            pkt++;
+        }
     }
-    else
-    {
-        cout << "Zle! Jest to jezyk programowania!";
 
-    }
-    Sleep(3000);
-    system("cls");
-
-
+    cout << imie << ", zdobyles " << pkt << " punktow w quzie." << endl;
 
     return 0;
 }
